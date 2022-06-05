41875e1337d64d1bd7239f6cfad641b4255322e0
emayorga1991 <eduardo.mayorga@hotmail.es>
2022-06-04 19:06:42 -0500

    product

--- app/routes/products/products.js
+++ app/routes/products/products.js
@@ -4,5 +4,7 @@ const productController = require('../../controllers/productController')
 
 router.get('/:category_id', productController.getProductsByCategoryId)
 router.get('/by_product_id/:product_id', productController.getProductsByProductId)
+router.post('/createProduct', productController.CreateProducts)
+router.put('/updateProduct', productController.putProduct)
 
 module.exports = router;
