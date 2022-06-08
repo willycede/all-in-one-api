e2b4718f8ea37c10e159b5b369b7b8d65a8c408d
emayorgacolmena1991 <107072122+emayorgacolmena1991@users.noreply.github.com>
2022-06-08 12:22:34 -0500

    actualizacion de consulta de productos por categorias

--- app/controllers/productController.js
+++ app/controllers/productController.js
@@ -3,30 +3,43 @@ const response = require('../config/response');
 
 const getProductsByCategoryId = async(req,res)=>{
     try {
+       
+        let validationObject ={}
+
         const category_id = req.params.category_id
         let errorMessage=''
-        if(category_id == null || category_id==undefined || category_id ==''){
+
+        if(category_id == null || category_id==undefined || category_id =='' || category_id ==0){
+
             errorMessage='El id de categoría no puede ser nulo o vacio'
             return response.error(req,res,{message:errorMessage, validationObject}, 422)
+
         }
-        const products = await productModel.getProductsByCategoryId({category_id})
+
+        const products = await productModel.getProductsByCategoryId(category_id)
         return response.success(req,res,products,200)
+
     } catch (error) {
         return response.error(req,res,{message:`getProductsByCategoryId: ${error.message}`},422)
     }
 }
 const getProductsByProductId = async(req,res)=>{
     try {
+
+        let validationObject ={}
         const product_id = req.params.product_id
+        
         let errorMessage=''
-        if(product_id == null || product_id==undefined || product_id ==''){
-            errorMessage='El id de producto no puede ser nulo o vacio'
+        if(product_id == null || product_id==undefined || product_id =='' || product_id == 0){
+
+            errorMessage='El id de producto no puede ser nulo,vacio o cero'
             return response.error(req,res,{message:errorMessage, validationObject}, 422)
+
         }
-        const product = await productModel.getProductsByProductId({product_id})
+        const product = await productModel.getProductsByProductId(product_id)
         return response.success(req,res,product,200)
     } catch (error) {
-        return response.error(req,res,{message:`getProductByProviderId: ${error.message}`},422)
+        return response.error(req,res,{message:`getProductsByProductId: ${error.message}`},422)
     }
 }
 const CreateProducts = async (req, res) => {
