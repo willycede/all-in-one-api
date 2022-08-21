b466bf02ae07a72802fc8c2d60e7d21ee5d28807
emayorga1991 <eduardo.eduardomayorga.mayorga@gmail.com>
2022-08-21 09:45:38 -0500

    Shpping car api-dos

--- app/models/products.js
+++ app/models/products.js
@@ -19,6 +19,26 @@ const putProductsUpdate = async ({ body }, trx) => {
 
 };
 
+const getAllProducts = async () => {
+    const queryToSearch ={
+        'status':generalConstants.STATUS_ACTIVE
+    }
+    const queryToExcecute = knex('products')
+        .where(queryToSearch);
+    return await queryToExcecute.orderBy('name', 'asc');
+}
+
+const getProductsByGeneralCategoryId = async (category_id) => {
+
+    const queryToExcecute = knex('products as p')
+    .leftJoin('features as f', 'f.id_products', 'p.id_products')
+    .join('category as cat', 'cat.id_category', 'f.id_category')
+    .select('p.id_products','p.cod_products','p.name','p.description', 'cat.id_category', 'p.price')
+    .where({ 'cat.id_general_category':category_id});
+    return await queryToExcecute.orderBy('p.name', 'asc');
+}
+
+/*
 const getProductsByCategoryId = async (category_id) => {
     return await knex('produtcs as p')
         .join('features as f', 'f.id_products', 'p.id_products')
@@ -27,7 +47,7 @@ const getProductsByCategoryId = async (category_id) => {
         .where({ 'cat.id_category':category_id})
         .orderBy('cat.name', 'asc');
 }
-
+*/
 const getProductsByProductId = async (id_products) => {
     return await knex.select()
         .from('products')
