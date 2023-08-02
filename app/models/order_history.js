1a1093b864731780ff6fba9107f40b3f952f0d6e
emayorga1991 <eduardo.eduardomayorga.mayorga@gmail.com~>
2023-08-01 22:44:30 -0500

    Cambios - cancelar pedido

--- app/models/order_history.js
+++ app/models/order_history.js
@@ -20,6 +20,24 @@ const getOrderHistory = async (id_user) => {
 };
 
 
+const deleteOrderHistoryModel = async (id_shopping_car, id_user, trx) => {
+
+    //console.log(id_shopping_car);
+
+    await (trx || knex)('shopping_car')
+    .where('id_shopping_car', '=', id_shopping_car)
+    .update(
+    {
+        status:3
+    });
+
+    return await getOrderHistory(id_user);
+
+
+};
+
+
 module.exports = {
-    getOrderHistory
+    getOrderHistory,
+    deleteOrderHistoryModel
 }
--- app/routes/order_history/order_history.js
+++ app/routes/order_history/order_history.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const orderHistoryController = require('../../controllers/orderHistoryController')
 
 router.get("/get_order_history/:id_user", orderHistoryController.getOrdersHistory);
+router.get("/delete_order_history/:id_shopping_car/:id_user", orderHistoryController.deleteHistory);
 
 
 module.exports = router;
